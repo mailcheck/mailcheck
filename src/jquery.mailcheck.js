0f06d3def8602d744e93eb5e79cca580f047acba
hpshelton <parker.shelton@jhu.edu>
2012-03-24 16:00:55 -0700

    Adding various email providers to the default email domain list
    Adding facebook.com, verizon.net, sbcglobal.net, att.net, gmx.com,
    mail.com

--- src/jquery.mailcheck.js
+++ src/jquery.mailcheck.js
@@ -13,7 +13,9 @@
 
 (function($){
   $.fn.mailcheck = function(opts, optsAlt) {
-    var defaultDomains = ["yahoo.com", "google.com", "hotmail.com", "gmail.com", "me.com", "aol.com", "mac.com", "live.com", "comcast.net", "googlemail.com", "msn.com", "hotmail.co.uk", "yahoo.co.uk"];
+    var defaultDomains = ["yahoo.com", "google.com", "hotmail.com", "gmail.com", "me.com", "aol.com", "mac.com",
+                          "live.com", "comcast.net", "googlemail.com", "msn.com", "hotmail.co.uk", "yahoo.co.uk",
+                          "facebook.com", "verizon.net", "sbcglobal.net", "att.net", "gmx.com", "mail.com"];
 
     if (typeof opts === 'object' && optsAlt === undefined) {
       // only opts is passed in
