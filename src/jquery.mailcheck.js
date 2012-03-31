bf47565e76abaea1adae1873da1475c0b32ef979
James Scott-Brown <james@jamesscottbrown.com>
2012-03-31 21:50:08 +0200

    Added some of the more popular UK ISP's mail domains

--- src/jquery.mailcheck.js
+++ src/jquery.mailcheck.js
@@ -15,7 +15,10 @@
   $.fn.mailcheck = function(opts, optsAlt) {
     var defaultDomains = ["yahoo.com", "google.com", "hotmail.com", "gmail.com", "me.com", "aol.com", "mac.com",
                           "live.com", "comcast.net", "googlemail.com", "msn.com", "hotmail.co.uk", "yahoo.co.uk",
-                          "facebook.com", "verizon.net", "sbcglobal.net", "att.net", "gmx.com", "mail.com"];
+                          "facebook.com", "verizon.net", "sbcglobal.net", "att.net", "gmx.com", "mail.com", 
+                          "btinternet.com", "virginmedia.com", "blueyonder.co.uk", "ntlworld.com", "virgin.net", 
+                          "tiscali.co.uk"; "talktalk.co.uk", "sky.com", "orange.net", "wanadoo.co.uk", 
+                          "freeserve.co.uk", "o2.co.uk"];
 
     if (typeof opts === 'object' && optsAlt === undefined) {
       // only opts is passed in
