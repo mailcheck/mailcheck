2984c11bc1df98d5bc41647b9fd7062632af2d9d
hpshelton <parker.shelton@jhu.edu>
2012-04-08 00:41:34 -0700

    Implement the Levenshtein distance algorithm

--- src/jquery.mailcheck.js
+++ src/jquery.mailcheck.js
@@ -8,7 +8,7 @@
  *
  * Licensed under the MIT License.
  *
- * v 1.1.0
+ * v 1.1.1
  */
  
 (function($){
@@ -29,8 +29,9 @@
       opts.domains = domains || defaultDomains;
     }
     opts.topLevelDomains = opts.topLevelDomains || defaultTopLevelDomains;
+    opts.distanceFunction = Kicksend.sift3Distance;
 
-    var result = Kicksend.mailcheck.suggest(encodeURI(this.val()), opts.domains, opts.topLevelDomains);
+    var result = Kicksend.mailcheck.suggest(encodeURI(this.val()), opts.domains, opts.topLevelDomains, opts.distanceFunction);
     if (result) {
       if (opts.suggested) {
         opts.suggested(this, result);
@@ -47,13 +48,13 @@ var Kicksend = {
   mailcheck : {
     threshold: 3,
 
-    suggest: function(email, domains, topLevelDomains) {
+    suggest: function(email, domains, topLevelDomains, distanceFunction) {
       email = email.toLowerCase();
 
       var emailParts = this.splitEmail(email);
 
-      var closestDomain = this.findClosestDomain(emailParts.domain, domains);
-
+      var closestDomain = this.findClosestDomain(emailParts.domain, domains, distanceFunction);
+      
       if (closestDomain) {
         if (closestDomain != emailParts.domain) {
           // The email address closely matches one of the supplied domains; return a suggestion
@@ -76,26 +77,29 @@ var Kicksend = {
       return false;
     },
 
-    findClosestDomain: function(domain, domains) {
+    findClosestDomain: function(domain, domains, distanceFunction) {
       var dist;
       var minDist = 99;
       var closestDomain = null;
 
-      if (!domains) {
+      if (!domain || !domains) {
         return false;
+      }
+      if(!distanceFunction) {
+        distanceFunction = this.sift3Distance;
       }
       
       for (var i = 0; i < domains.length; i++) {
         if (domain === domains[i]) {
           return domain;
         }
-        dist = this.stringDistance(domain, domains[i]);
+        dist = distanceFunction(domain, domains[i]);
         if (dist < minDist) {
           minDist = dist;
           closestDomain = domains[i];
         }
       }
-
+      
       if (minDist <= this.threshold && closestDomain !== null) {
         return closestDomain;
       } else {
@@ -103,7 +107,7 @@ var Kicksend = {
       }
     },
 
-    stringDistance: function(s1, s2) {
+    sift3Distance: function(s1, s2) {
       // sift3: http://siderite.blogspot.com/2007/04/super-fast-and-accurate-string-distance.html
       if (s1 == null || s1.length === 0) {
         if (s2 == null || s2.length === 0) {
@@ -144,7 +148,46 @@ var Kicksend = {
       }
       return (s1.length + s2.length) /2 - lcs;
     },
-
+    
+    levenshteinDistance: function(s, t) {
+      // Determine the Levenshtein distance between s and t
+      if (!s || !t) {
+        return 99;
+      }     
+      
+      var m = s.length;
+      var n = t.length;
+      
+      /* For all i and j, d[i][j] holds the Levenshtein distance between
+       * the first i characters of s and the first j characters of t.
+       * Note that the array has (m+1)x(n+1) values.
+       */
+      var d = new Array();
+      for (var i = 0; i <= m; i++) {
+        d[i] = new Array();
+        d[i][0] = i;
+      }
+      for (var j = 0; j <= n; j++) {
+        d[0][j] = j
+      }
+            
+      // Determine substring distances
+      for (var j = 1; j <= n; j++) {
+        for (var i = 1; i <= m; i++) {
+          if (s[i-1] == t[j-1]) { // Subtract one to start at strings' index zero instead of index one
+            d[i][j] = d[i-1][j-1];
+          } else {
+            d[i][j] = Math.min(d[i-1][j] + 1,            // deletion
+                               Math.min(d[i][j-1] + 1,   // insertion
+                                        d[i-1][j-1] + 1) // substitution
+                              );
+          }
+        }
+      }
+      // Return the strings' distance
+      return d[m][n];
+    },
+    
     splitEmail: function(email) {
       var parts = email.split('@');
 
